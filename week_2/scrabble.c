aeb28c3cdf326a735f61c907fcc02c13351ebf87
nicole kim <125776853+qqnk@users.noreply.github.com>
2023-06-05 04:50:06 -0400

    Update scrabble.c

--- week_2/scrabble.c
+++ week_2/scrabble.c
@@ -18,7 +18,7 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO 1: Print the winner
+    // TODO #1: Print the winner
     if (score1 > score2)
     {
         printf("player 1 wins\n");
@@ -35,7 +35,7 @@ int main(void)
 
 int compute_score(string word)
 {
-    // TODO 2: Compute and return score for string
+    // TODO #2: Compute and return score for string
     int score = 0;
 
     for (int i = 0, len = strlen(word); i < len; i++)
